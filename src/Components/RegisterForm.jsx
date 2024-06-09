5f07f40021c3f1c40947b49d04abe86cebb652d7
Derya Akan <d.akan369@gmail.com>
2024-06-09 15:08:50 +0300

    Sign Up form css

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -21,46 +21,43 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className="flex w-full flex-col items-center px-24 py-20 justify-center text-[#252B42] sm:justify-center sm:p-0">
-      <div className="flex justify-center flex-wrap sm:gap-5 shadow-md py-10 pr-24 pl-20 bg-white rounded">
-        <div className="flex justify-start w-4/5 py-5 flex-wrap sm:gap-5">
-          <div className="flex basis-2/3 sm:flex-wrap sm:gap-5">
-            <div className="w-full flex flex-col items-center relative text-gray-700 text-left gap-10 sm:gap-2">
-              <h2 className="font-bold text-2xl">Create Account</h2>
-              <form className="flex flex-col gap-10 text-sm text-[#737373] sm:gap-2">
-                <input
-                  className="text-sm max-w-fit text-[#737373] shadow-md py-2 my-2 px-4"
-                  type="text"
-                  id="username"
-                  placeholder="Enter username"
-                />
-                <input
-                  className="text-sm max-w-fit text-[#737373] shadow-md py-2 my-2 px-4"
-                  type="email"
-                  id="email"
-                  placeholder="Enter email"
-                />
+    <div className="flex flex-col justify-center flex-wrap text-[#737373] sm:gap-5 shadow-lg py-10 px-20 bg-white rounded">
+      <div className="flex flex-col gap-3">
+        <h2 className="font-bold text-2xl text-[#0EB39E]">Crowdie</h2>
+        <p>Discover your crowd on Crowdie !</p>
+      </div>
+      <div className="flex justify-start py-5 flex-wrap sm:gap-5">
+        <div className="w-full flex flex-col items-center relative text-gray-700 text-left gap-10 sm:gap-2">
+          <h3 className="text-xl">Create Account</h3>
+          <form className="flex flex-col gap-10 text-sm sm:gap-2">
+            <input
+              className="text-sm max-w-fit rounded-sm border border-[#0EB39E] hover:border-[#4CCCE6] py-2 my-2 px-4"
+              type="text"
+              id="username"
+              placeholder="Enter username"
+            />
+            <input
+              className="text-sm max-w-fit rounded-sm border border-[#0EB39E] hover:border-[#4CCCE6] py-2 my-2 px-4"
+              type="email"
+              id="email"
+              placeholder="Enter email"
+            />
 
-                <input
-                  className="text-sm max-w-fit text-[#737373] shadow-md py-2 my-2 px-4"
-                  type="password"
-                  id="password"
-                  placeholder="Enter password"
-                />
-
-                <input
-                  className="text-sm max-w-fit text-[#737373] shadow-md py-2 my-2 px-4"
-                  type="password"
-                  placeholder="Validate password"
-                />
-
-                <button className="bg-gradient-to-r from-[#0EB39E] to-[#4CCCE6] font-bold text-white text-sm py-2 px-8 rounded hover:bg-[#23A6F0]">
-                  Sign In
-                </button>
-              </form>
-            </div>
-          </div>
+            <input
+              className="text-sm max-w-fit rounded-sm border border-[#0EB39E] hover:border-[#4CCCE6] py-2 my-2 px-4"
+              type="password"
+              id="password"
+              placeholder="Enter password"
+            />
+            <button className="bg-gradient-to-r from-[#0EB39E] to-[#4CCCE6] font-bold text-white text-sm py-2 px-8 rounded hover:bg-[#23A6F0]">
+              Sign Up
+            </button>
+          </form>
         </div>
+      </div>
+      <div className="flex flex-col text-[#737373] px-4">
+        <p>Alreay have an acoount?</p>
+        <a className="text-[#0EB39E] cursor-pointer underline">Login</a>
       </div>
     </div>
   );
